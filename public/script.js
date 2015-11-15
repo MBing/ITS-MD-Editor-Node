924934f6b16c2cc6c7c82e9d75d395cc4d38dfe9
Martin Bing <bing.martin.photography@gmail.com>
2015-11-15 19:28:50 +0100

    Back to basic edits

--- public/script.js
+++ public/script.js
@@ -1,11 +1,7 @@
-/* public/script.js */
-
 window.onload = function() {
     var converter = new showdown.Converter();
     var pad = document.getElementById('pad');
-    var markdownArea = document.getElementById('markdown');
-    var socket = new BCSocket(null, {reconnect: true}),
-        sjs = new sharejs.Connection(socket);
+    var markdownArea = document.getElementById('markdown'); 
 
     // make the tab act like a tab
     pad.addEventListener('keydown',function(e) {
@@ -58,27 +54,13 @@ window.onload = function() {
     pad.addEventListener('input', convertTextAreaToMarkdown);
 
     // ignore if on home page
-    // if(document.location.pathname.length < 2){
+    if(document.location.pathname.length < 2){
         // implement share js
         sharejs.open(document.location.pathname, 'text', function(error, doc) {
             doc.attach_textarea(pad);
-            // doc.attachTextarea(pad);
             convertTextAreaToMarkdown();
         });        
-    // }
-    // var doc = sjs.get('docs', document.location.pathname);
-
-    // // Subscribe to changes
-    // doc.subscribe();
-
-    // // This will be called when we have a live copy of the server's data.
-    // doc.whenReady(function() {
-    //   console.log('doc ready, data: ', doc.getSnapshot());
-
-    //   // Create a JSON document with value x:5
-    //   if (!doc.type) doc.create('text');
-    //   doc.attachTextarea(pad);
-    // });
+    }
 
     // convert on page load
     convertTextAreaToMarkdown();
--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 /* server.js */
 
-var express = require('express'),
-	app = express(),
-	redis = require('redis'),
-	// client = redis.createClient(),
-	sharejs = require('share');
+var express = require('express');
+var app = express();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -21,27 +18,18 @@ app.get('/(:id)', function(req, res) {
 });
 
 // get sharejs dependencies
-var sharejs = require('share').server;
+var sharejs = require('share');
 
-// // set up redis server
+// set up redis server
 var redisClient;
 console.log(process.env.REDISTOGO_URL);
 if (process.env.REDISTOGO_URL) {
-	console.log('client exists');
   var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-  console.log('rtg ', rtg);
   redisClient = require("redis").createClient(rtg.port, rtg.hostname);
   redisClient.auth(rtg.auth.split(":")[1]);
-  console.log('password', rtg.auth.split(":")[1]);
-  console.log(rtg.auth.split(":")[1] === 'd9b9b842d5072522ade847cb0ed207a8');
 } else {
   redisClient = require("redis").createClient();
 }
-
-// client.on("error", function (err) {
-//     console.log("Error " + err);
-// });
-
 
 // options for sharejs 
 var options = {
@@ -49,7 +37,7 @@ var options = {
 };
 
 // attach the express server to sharejs
-sharejs.attach(app, options);
+sharejs.server.attach(app, options);
 
 // listen on port 8000 (for localhost) or the port defined for heroku
 var port = process.env.PORT || 8000;
