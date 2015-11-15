87ffab2386e845f5fb8bb30b1b20d262d0504448
Martin Bing <bing.martin.photography@gmail.com>
2015-11-15 17:26:16 +0100

    Update sharejs and syntax client server connection

--- public/script.js
+++ public/script.js
@@ -3,7 +3,9 @@
 window.onload = function() {
     var converter = new showdown.Converter();
     var pad = document.getElementById('pad');
-    var markdownArea = document.getElementById('markdown'); 
+    var markdownArea = document.getElementById('markdown');
+    var socket = new BCSocket(null, {reconnect: true}),
+        sjs = new sharejs.Connection(socket);
 
     // make the tab act like a tab
     pad.addEventListener('keydown',function(e) {
@@ -56,13 +58,27 @@ window.onload = function() {
     pad.addEventListener('input', convertTextAreaToMarkdown);
 
     // ignore if on home page
-    if(document.location.pathname.length < 2){
-        // implement share js
-        sharejs.open(document.location.pathname, 'text', function(error, doc) {
-            doc.attach_textarea(pad);
-            convertTextAreaToMarkdown();
-        });        
-    }
+    // if(document.location.pathname.length < 2){
+    //     // implement share js
+    //     sharejs.open(document.location.pathname, 'text', function(error, doc) {
+    //         // doc.attach_textarea(pad);
+    //         doc.attachTextarea(pad);
+    //         convertTextAreaToMarkdown();
+    //     });        
+    // }
+    var doc = sjs.get('docs', document.location.pathname);
+
+    // Subscribe to changes
+    doc.subscribe();
+
+    // This will be called when we have a live copy of the server's data.
+    doc.whenReady(function() {
+      console.log('doc ready, data: ', doc.getSnapshot());
+
+      // Create a JSON document with value x:5
+      if (!doc.type) doc.create('text');
+      doc.attachTextarea(pad);
+    });
 
     // convert on page load
     convertTextAreaToMarkdown();
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 /* server.js */
 
-var express = require('express');
-var app = express();
+var express = require('express'),
+	app = express(),
+	redis = require('redis'),
+	// client = redis.createClient(),
+	sharejs = require('share');
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -18,18 +21,27 @@ app.get('/(:id)', function(req, res) {
 });
 
 // get sharejs dependencies
-var sharejs = require('share');
+var sharejs = require('share').server;
 
-// set up redis server
+// // set up redis server
 var redisClient;
 console.log(process.env.REDISTOGO_URL);
 if (process.env.REDISTOGO_URL) {
+	console.log('client exists');
   var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  console.log('rtg ', rtg);
   redisClient = require("redis").createClient(rtg.port, rtg.hostname);
   redisClient.auth(rtg.auth.split(":")[1]);
+  console.log('password', rtg.auth.split(":")[1]);
+  console.log(rtg.auth.split(":")[1] === 'd9b9b842d5072522ade847cb0ed207a8');
 } else {
   redisClient = require("redis").createClient();
 }
+
+// client.on("error", function (err) {
+//     console.log("Error " + err);
+// });
+
 
 // options for sharejs 
 var options = {
@@ -37,7 +49,7 @@ var options = {
 };
 
 // attach the express server to sharejs
-sharejs.server.attach(app, options);
+sharejs.attach(app, options);
 
 // listen on port 8000 (for localhost) or the port defined for heroku
 var port = process.env.PORT || 8000;
