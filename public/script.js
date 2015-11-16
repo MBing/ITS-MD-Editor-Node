2770c0d0501048b63a5d2376e45e49895fc677ea
Martin Bing <bing.martin.photography@gmail.com>
2015-11-16 13:23:52 +0100

    Hide auth stuff client

--- public/script.js
+++ public/script.js
@@ -6,46 +6,46 @@ window.onload = function() {
     var markdownArea = document.getElementById('markdown');
 
     /* AUTH STUFF */
-    var lock = new Auth0Lock('VZNkO2UEcWKgJmMQGjpb03WBzKq9SwCa', 'app43832868.eu.auth0.com');
+    // var lock = new Auth0Lock('VZNkO2UEcWKgJmMQGjpb03WBzKq9SwCa', 'app43832868.eu.auth0.com');
 
-    var userProfile = null;
+    // var userProfile = null;
 
-    document.getElementById('btn-login').addEventListener('click', function() {
-        lock.show({ authParams: { scope: 'openid' } });
-    });
-    var hash = lock.parseHash(window.location.hash);
+    // document.getElementById('btn-login').addEventListener('click', function() {
+    //     lock.show({ authParams: { scope: 'openid' } });
+    // });
+    // var hash = lock.parseHash(window.location.hash);
 
-    if (hash && hash.id_token) {
-      //save the token in the session:
-      localStorage.setItem('id_token', hash.id_token);
-    }
+    // if (hash && hash.id_token) {
+    //   //save the token in the session:
+    //   localStorage.setItem('id_token', hash.id_token);
+    // }
 
-    if (hash && hash.error) {
-      alert('There was an error: ' + hash.error + '\n' + hash.error_description);
-    }
-    //retrieve the profile:
-    var id_token = localStorage.getItem('id_token');
-    if (id_token) {
-      lock.getProfile(id_token, function (err, profile) {
-        if (err) {
-          return alert('There was an error geting the profile: ' + err.message);
-        }
-        document.getElementById('name').textContent = profile.name;
-      });
-    }
-    var getFoos = fetch('/', {
-      headers: {
-        'Authorization': 'Bearer ' + localStorage.getItem('id_token')
-      },
-      method: 'GET',
-      cache: false
-    });
+    // if (hash && hash.error) {
+    //   alert('There was an error: ' + hash.error + '\n' + hash.error_description);
+    // }
+    // //retrieve the profile:
+    // var id_token = localStorage.getItem('id_token');
+    // if (id_token) {
+    //   lock.getProfile(id_token, function (err, profile) {
+    //     if (err) {
+    //       return alert('There was an error geting the profile: ' + err.message);
+    //     }
+    //     document.getElementById('name').textContent = profile.name;
+    //   });
+    // }
+    // var getFoos = fetch('/', {
+    //   headers: {
+    //     'Authorization': 'Bearer ' + localStorage.getItem('id_token')
+    //   },
+    //   method: 'GET',
+    //   cache: false
+    // });
 
-    getFoos.then(function (response) {
-      response.json().then(function (foos) {
-        console.log('the foos:', foos);
-      });
-    });
+    // getFoos.then(function (response) {
+    //   response.json().then(function (foos) {
+    //     console.log('the foos:', foos);
+    //   });
+    // });
     /* END OF AUTH STUFF */
 
     // make the tab act like a tab
